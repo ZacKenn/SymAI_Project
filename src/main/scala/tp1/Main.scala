afe6c5d4fbdd151a3b86f72bf5fa9b8f14f141fb
Vincentlin421 <vincentlin421@gmail.com>
2022-01-25 16:49:51 +0100

    add statement

--- src/main/scala/tp1/Main.scala
+++ src/main/scala/tp1/Main.scala
@@ -6,17 +6,8 @@ import com.github.javafaker.service.{FakeValuesService, RandomService}
 import java.util.Locale
 
 object Main extends App {
-  val test = new Test("file:///home/lim/M2 S1/SymAI_Project_Scala/lubm1.ttl")
-
+  val test = new Test("file:lubm1.ttl")
   val f = new Faker()
-//  val fvs = new FakeValuesService(
-//    new Locale("en-GB"), new RandomService())
-
-
-
-
   test.load()
-  test.showModel()
-  println(test.size())
-  test.generateRDF()
+  test.addStatement()
 }
--- src/main/scala/tp1/Test.scala
+++ src/main/scala/tp1/Test.scala
@@ -1,12 +1,24 @@
 package tp1
 
+import com.github.javafaker.Faker
 import org.apache.jena.rdf.model._
 
+import java.util.Locale
 import scala.collection.convert.ImplicitConversions.`list asScalaBuffer`
 import scala.collection.mutable.ListBuffer
 
 class Test(val dbSource : String) {
   val model = ModelFactory.createDefaultModel();
+  val identifierRDF = model.createProperty("http://swat.cse.lehigh.edu/onto/univ-bench.owl#identifier")
+  val firstNameRDF = model.createProperty("http://swat.cse.lehigh.edu/onto/univ-bench.owl#firstName")
+  val lastNameRDF = model.createProperty("http://swat.cse.lehigh.edu/onto/univ-bench.owl#lastName")
+  val genderRDF = model.createProperty("http://swat.cse.lehigh.edu/onto/univ-bench.owl#gender")
+  val zipcodeRDF = model.createProperty("http://swat.cse.lehigh.edu/onto/univ-bench.owl#zipcode")
+  val dateOfBirthRDF = model.createProperty("http://swat.cse.lehigh.edu/onto/univ-bench.owl#dateOfBirth")
+  val dateOfVaccination = model.createProperty("http://swat.cse.lehigh.edu/onto/univ-bench.owl#dateOfVaccination")
+  val vaccineName = model.createProperty("http://swat.cse.lehigh.edu/onto/univ-bench.owl#vaccineName")
+  val f = new Faker(new Locale("fr"));
+  val vaccine = List( "Pfizer", "Moderna", "AstraZeneca", "SpoutnikV", "CanSinoBio")
 
   def load() = model.read(dbSource, "TTL")
   def showModel() : Unit = println("is empty ? "  + model.isEmpty())
@@ -20,19 +32,22 @@ class Test(val dbSource : String) {
     props.toList.distinct
   }
 
-  def generateRDF() = {
-    val identifierRDF = model.createProperty("http://swat.cse.lehigh.edu/onto/univ-bench.owl#identifier")
-    val firstNameRDF = model.createProperty("http://swat.cse.lehigh.edu/onto/univ-bench.owl#firstName")
-    val lastNameRDF = model.createProperty("http://swat.cse.lehigh.edu/onto/univ-bench.owl#lastName")
-    val genderRDF = model.createProperty("http://swat.cse.lehigh.edu/onto/univ-bench.owl#gender")
-    val zipcodeRDF = model.createProperty("http://swat.cse.lehigh.edu/onto/univ-bench.owl#zipcode")
-    val dateOfBirthRDF = model.createProperty("http://swat.cse.lehigh.edu/onto/univ-bench.owl#dateOfBirth")
-
-//    getDistinctSubjects().foreach(x => println(x))
-    val typeProperty = "http://swat.cse.lehigh.edu/onto/univ-bench.owl#teacherOf"
+  def addStatement() = {
+    val typeProperty = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
     val rdfType = model.createProperty(typeProperty)
-    val it = model.listSubjectsWithProperty(rdfType)
-    it.toList.distinct.foreach(x => println(x))
+    val fullprofessor = model.createResource("http://swat.cse.lehigh.edu/onto/univ-bench.owl#FullProfessor")
+    val it = model.listSubjectsWithProperty(rdfType,fullprofessor)
+    it.toList.distinct.foreach(x => {
+      model.createStatement(x,identifierRDF,model.createResource(""+f.number()))
+      model.createStatement(x,firstNameRDF,model.createResource(f.name().firstName()))
+      model.createStatement(x,lastNameRDF,model.createResource(f.name().lastName()))
+      model.createStatement(x,genderRDF,model.createResource(f.regexify("[FM]{1}")))
+      model.createStatement(x,zipcodeRDF,model.createResource(""+f.address().zipCode()))
+      model.createStatement(x,dateOfBirthRDF,model.createResource(""+f.date().birthday(30,71)))
+      model.createStatement(x,dateOfVaccination,model.createResource(""+f.date().birthday(0,3)))
+      model.createStatement(x,vaccineName,model.createResource(vaccine(f.number().numberBetween(0,5))))
+    })
+
   }
 
 
